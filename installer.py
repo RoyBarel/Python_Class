df2fab6150ec116357571dcfea721c44444d5ed4
RoyBarel <48448944+RoyBarel@users.noreply.github.com>
2019-04-26 13:06:25 +0300

    Configure httpd And adding Functions

--- installer.py
+++ installer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env ptrhon3.6
+#!/usr/bin/env python3.6
 
 #########################################################
 #created by RoyBarel
@@ -34,50 +34,101 @@ os.system('sudo yum -y install php php-mbstring php-pear ')
 
 #Functions /\/\/\/\/\//\/\/\/\/\/\/\/\/\/\/\/\/\/\/\//\/\/\
 
+def changingFiles (input_file, in_search_term , in_new_data , start_line_search=0):
+	
+	try:
+		search_term = in_search_term+"\n"
+		new_data = in_new_data+"\n"
 
+		try:
+			index = lines.index(search_term , start_line_search)
+			lines[index] = new_data
+			f = open(input_file,'w')
+			f.writelines(lines)
+			f.close()
+						
+		except ValueError:
+			print("Not find the term")
+	except FileNotFoundError:
+		print("Not find the file, check the Path of the file")
+	except PermissionError:
+		print("You don't have permission to get this file")
+	except Exception as e:
+		print(e)
+		sys.exit(1)
+
+
+def addingToTheLast (input_file , in_new_data ):
+	
+	try:
+		with open(input_file) as f1: # open the file 
+				lines = f1.readlines() # Copy the file to lists
+		new_data = in_new_data+"\n"
+		f = open(input_file,'w')
+		f.writelines(lines)
+		f.write(new_data)
+		f.close()
+		
+	except FileNotFoundError:
+		print("Not find the file, check the Path of the file")
+	except PermissionError:
+		print("You don't have permission to get this file")
+	except Exception as e:
+		print(e)
+		sys.exit(1)
+	
 
 ###
 #Main - _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _- _
 ###
 
 if __name__ == '__main__':
+	
 	#creating directory for each language
 	os.system('sudo mkdir /var/www/html/cgi-enabled/')
 	os.system('sudo mkdir /var/www/html/php/')
-	
 	
 	try :
 		#Install httpd
 		os.system('sudo yum -y install httpd')
 		os.system('sudo rm -f /etc/httpd/conf.d/welcome.conf') 
 		
-		#Configure httpd
-		#os.system('cp /etc/httpd/conf/httpd.conf ~/')
-		with open('/etc/httpd/conf/httpd.conf','r') as web:
-			httpd_file_data = web.readlines()
-			#print(httpd_file_data)
-	
-		search_term = "^ServerAdmin root@localhost"
-		new_data = 'ServerAdmin roy@localhost'
-		time.sleep(1)
-		for line in httpd_file_data:
-			#print(line)
-			if re.findall(search_term, line):
-				print(line)
-				time.sleep(1)
-				line = new_data
-				#print(line)
-		with open('/etc/httpd/conf/httpd.conf','r') as f:
 			
-			print(str(httpd_file_data))
-			f.write(str(httpd_file_data))
-			#	if line == None:
-			#		print('no matches found')
+		#Configure httpd
+		## Copy the file httpd.conf to tmp file
 		
+		with open('/etc/httpd/conf/httpd.conf') as f1: # open the file 
+				lines = f1.readlines() # Copy the file to lists
+		tmpFileHttpd = '/tmp/tmpHttpd1.conf'
+		f = open(tmpFileHttpd,'w')
+		f.writelines(lines)
+		f.close()
+		
+		
+		changingFiles(tmpFileHttpd, "ServerAdmin root@localhost" , 'ServerAdmin roy@localhost')
+		
+		print("The admin's email address is: roy@localhost")
+		
+		changingFiles(tmpFileHttpd, "#ServerName www.example.com:80" , "ServerName 127.0.0.1:80" )
+		
+		print("The ServerName is: 127.0.0.1:80")
+		
+		changingFiles(tmpFileHttpd, "    AllowOverride None" , "    AllowOverride All" , 150)
+		
+		changingFiles( tmpFileHttpd, "    DirectoryIndex index.html" , "    DirectoryIndex index.html index.php index.perl index.py" , 160)
+				
+		addingToTheLast(tmpFileHttpd , "ServerTokens Prod" )
+				
+		addingToTheLast(tmpFileHttpd , "KeepAlive On" )
 	
-	
+		#return the file to the origain place
+		os.system(f'sudo mv {tmpFileHttpd} /etc/httpd/conf/httpd.conf')
+		
+		
+		#https://www.server-world.info/en/note?os=CentOS_7&p=httpd&f=1
+		
 	except Exception as e:
 		print(e)
 		sys.exit(1)
-
 	
+		
